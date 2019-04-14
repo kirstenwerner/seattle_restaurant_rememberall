4656fbd4d32563852bcd79a2b14483d87a7f2a7f
kirstenwerner <kirstenmwerner@gmail.com>
2019-04-14 18:28:18 -0500

    added slug method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :restaurants
+
+  def slug
+      self.username.downcase.gsub(/[^0-9a-z\- ]/, "").gsub(" ", "-")
+   end
+
+   def self.find_by_slug(name)
+      user = User.find {|user| user.slug == name}
+   end
 end
