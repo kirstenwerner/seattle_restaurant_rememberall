cd2f34bbabf6cecfad801e35abcf28ba2a3c78fc
kirstenwerner <kirstenmwerner@gmail.com>
2019-04-17 00:24:39 -0700

    update User class relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :restaurants
+  has_many :cuisines, through: :restaurants
+  has_many :neighborhoods, through: :restaurants 
 
   def slug
       self.username.downcase.gsub(/[^0-9a-z\- ]/, "").gsub(" ", "-")
