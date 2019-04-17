484237362634103c8108c6816e6bf4164e183cce
kirstenwerner <kirstenmwerner@gmail.com>
2019-04-17 00:31:14 -0700

    re-work restaurants table

--- db/migrate/20190413002914_create_restaurants.rb
+++ db/migrate/20190413002914_create_restaurants.rb
@@ -2,9 +2,9 @@ class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :cuisine
-      t.string :neighborhood
       t.integer :user_id
+      t.integer :cuisine_id
+      t.integer :neighborhood_id
     end
   end
 end
